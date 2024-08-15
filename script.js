ce8b449a49651b78d384160cd8f3e458e2fa46b9
Miroslav <mir.vbok@mail.ru>
2024-08-15 19:29:48 +0700

    на всякий случай

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
-let currentClass = null;
-let editCurrentClass = null;
-let editCurrentTask = null;
-let selectedValue;
-//BUTTONS
+//#region Глобальные-перменные
+let currentTaskList = null;
+//#endregion
+//#region Кнопки
 const addNewClassBtn = document.querySelector(".addNewClass");
 const dropDownBtn = document.querySelector(".dropDownBtn");
 const submitClassBtn = document.querySelector(".classSubmit");
 const themeToggle = document.querySelector(".themeToggle");
-//
-//FORMS
+const submitTaskBtn = document.querySelector(".submitTaskBtn");
+//#endregion
+//#region Формы
 const allForms = document.querySelectorAll("form");
 
 const taskData = document.forms.taskInfoModal;
@@ -22,7 +22,7 @@ const editClassBG = classData.elements.selectClassBG;
 const enlargeTaskContainer = document.forms.enlargeTaskInfo;
 const enlargeTaskTitle = enlargeTaskContainer.elements.enlargeTaskTitle;
 const enlargeTaskNote = enlargeTaskContainer.elements.enlargeTaskNote;
-//
+//#endregion
 //MODALS
 const classBGSelector = document.querySelector('select[name="selectClassBG"]');
 
@@ -31,14 +31,7 @@ const mainContent = document.getElementById("root");
 const modalCover = document.querySelectorAll(".modalCover");
 const body = document.body;
 //
-const checkValidation = (attribute) => {
-  if (attribute.checkValidity()) {
-    submitBtn.removeAttribute("disabled");
-  } else {
-    submitBtn.setAttribute("disabled", "true");
-  }
-};
-
+//#region классы
 const openDropdown = () => {
   addBlock.classList.toggle("show");
   mainContent.classList.toggle("shiftDown");
@@ -53,12 +46,22 @@ const createNewClass = (name) => {
 const addNewClass = () => {
   const name = document.querySelector(".newClassName").value;
   const userClass = createNewClass(name);
-  const editBtn = userClass.querySelector(".editUserHeader");
+  const editClassBtn = userClass.querySelector(".editUserHeader");
+  editClassBtn.addEventListener("click", () => {
+    const editCurrentClass = event.target.closest(".userClassContainer");
+    openEditClassModal(editCurrentClass);
+    openModal();
+  });
   const addTaskBtn = userClass.querySelector(".addTask");
-  mainContent.appendChild(userClass);
-  editBtn.addEventListener("click", openModal);
+  addTaskBtn.addEventListener("click", () => {
+    currentTaskList = event.target
+      .closest(".userClassContainer")
+      .querySelector(".userTasksList");
+  });
   addTaskBtn.addEventListener("click", openModal);
+  mainContent.appendChild(userClass);
 };
+//#endregion
 
 const createNewTask = (title, note) => {
   const template = document.querySelector(".newTask");
@@ -67,68 +70,62 @@ const createNewTask = (title, note) => {
   newTask.querySelector(".taskNote").textContent = note;
   return newTask;
 };
-const addNewTask = (userClass) => {
+const addNewTask = () => {
   event.preventDefault();
   const title = taskTitle.value;
   const note = taskNote.value;
 
   const userTask = createNewTask(title, note);
   const enlargeBtn = userTask.querySelector(".enlargeTaskBtn");
-  const currentClass = userClass.querySelector(".userTasksList");
-  currentClass.appendChild(userTask);
-
   const addSubTaskBtn = userTask.querySelector(".addTask");
+
+  currentTaskList.appendChild(userTask);
 
   enlargeBtn.addEventListener("click", openModal);
 };
-
+//#region modals
 const openModal = () => {
   const currentBtn = event.target;
   const modalType = currentBtn.dataset.modal;
   const needToBeOpened = document.getElementById(modalType);
   needToBeOpened.classList.add("opened");
-  switch (modalType) {
-    case "editClassName":
-      openEditClassModal();
-      break;
-    case "enlargeTaskInfo":
-      openEditTaskModal();
-      break;
-  }
 };
 const closeModal = () => {
   const openedModal = event.target.closest(".opened");
   openedModal.classList.remove("opened");
 };
-const openEditClassModal = () => {
-  const editClass = event.target.closest(".userClassContainer");
-  const editTitle = editClass.querySelector(".className");
+
+//#endregion
+
+//#region editModals
+const openEditClassModal = (editCurrentClass) => {
+  const editTitle = editCurrentClass.querySelector(".className");
   editClassTitle.value = editTitle.textContent;
-
-  const submitClassBtn = document.querySelector(".classSubmit");
-  submitClassBtn.addEventListener("click", () =>
-    submitClassEdit(editTitle, editClass, selectedValue)
-  );
+  submitClassBtn.addEventListener("click", () => {
+    submitClassEdit(editTitle);
+  });
+  openModal();
 };
-const submitClassEdit = (editTitle, editClass, selectedValue) => {
-  editTitle.textContent = editClassTitle.value;
-  editClass.classList.add(selectedValue);
-};
-
 const openEditTaskModal = () => {
   const editTask = event.target.closest(".userTaskContainer");
   const editTitle = editTask.querySelector(".taskTitle");
   const editNote = editTask.querySelector(".taskNote");
-
-  const submitTaskBtn = document.querySelector(".taskSubmit");
   submitTaskBtn.addEventListener("click", () =>
     submitTaskEdit(editTitle, editNote)
   );
+  openModal();
+};
+//#endregion
+
+//#region сабмиты
+const submitClassEdit = (editTitle) => {
+  editTitle.textContent = editClassTitle.value;
 };
 const submitTaskEdit = (editTitle, editNote) => {
   editTitle.textContent = enlargeTaskTitle.value;
   editNote.textContent = enlargeTaskNote.value;
 };
+//#endregion
 
 allForms.forEach((form) => {
   form.addEventListener("click", () => {
@@ -147,5 +144,6 @@ themeToggle.addEventListener("click", () => {
 classBGSelector.addEventListener("change", () => {
   selectedValue = event.target.value;
 });
+submitTaskBtn.addEventListener("click", addNewTask);
 dropDownBtn.addEventListener("click", openDropdown);
 addNewClassBtn.addEventListener("click", addNewClass);
